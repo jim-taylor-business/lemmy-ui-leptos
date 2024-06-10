35973407d2a955426441e443078552ec176d2d24
jim-taylor-business <jim.taylor.business@gmail.com>
2024-06-10 13:40:52 +0100

    initial about

--- src/ui/components/common/mod.rs
+++ src/ui/components/common/mod.rs
@@ -1,3 +1,5 @@
 pub mod icon;
 pub mod nav;
 pub mod text_input;
+
+pub mod about;
--- src/ui/components/communities/communities_activity.rs
+++ src/ui/components/communities/communities_activity.rs
@@ -1,9 +1,12 @@
 use leptos::*;
 
+use crate::ui::components::common::about::About;
+
 #[component]
 pub fn CommunitiesActivity() -> impl IntoView {
   view! {
     <main class="mx-auto">
+      <About site_signal=RwSignal::new(None) />
       <h2 class="p-6 text-4xl">"Communities page"</h2>
     </main>
   }
