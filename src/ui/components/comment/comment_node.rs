a5e9ddb521c2c3de752a7f991a26de1081e82a9e
jim-taylor-business <jim.taylor.business@gmail.com>
2024-06-12 19:43:59 +0100

    bug

--- src/ui/components/comment/comment_node.rs
+++ src/ui/components/comment/comment_node.rs
@@ -7,19 +7,27 @@ pub fn CommentNode(
   comments: MaybeSignal<Vec<CommentView>>,
   level: usize,
 ) -> impl IntoView {
-  let mut comments_clone = comments.get().clone();
+  let mut comments_descendants = comments.get().clone();
   let id = comment_view.get().comment.id.to_string();
-  comments_clone.retain(|ct| {
+
+  let mut comments_children: Vec<CommentView> = vec![];
+
+  comments_descendants.retain(|ct| {
     let tree = ct.comment.path.split('.').collect::<Vec<_>>();
-    if tree.len() > level + 1 {
+    if tree.len() == level + 2 {
+      if tree.get(level).unwrap_or(&"").eq(&id) {
+        comments_children.push(ct.clone());
+      }
+      false
+    } else if tree.len() > level + 2 {
       tree.get(level).unwrap_or(&"").eq(&id)
     } else {
       false
     }
   });
-  let com_sig = RwSignal::new(comments_clone);
-
-  // if level > 3 { return view! { <div> </div> }; }
+  
+  let com_sig = RwSignal::new(comments_children);
+  let des_sig = RwSignal::new(comments_descendants);
 
   view! {
     <div class="pl-4">
@@ -29,7 +37,7 @@ pub fn CommentNode(
         <span class="font-bold"> { comment_view.get().creator.name }</span>
       </div>
       <For each=move || com_sig.get() key=|cv| cv.comment.id let:cv>
-        <CommentNode comment_view=cv.into() comments=comments.get().into() level=level + 1/>
+        <CommentNode comment_view=cv.into() comments=des_sig.get().into() level=level + 1/>
       </For>
     </div>
   }
